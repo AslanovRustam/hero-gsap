4e51521d22fa1004050ca4b0cd445ad7faa49ba2
AslanovRustam <aslanovrg@gmail.com>
2025-05-27 23:38:27 +0300

    update quotes

--- src/app/page.js
+++ src/app/page.js
@@ -163,14 +163,14 @@ export default function Home() {
               <div ref={introCopytRef}>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
                   standard dummy text ever since the 1500s, when an unknown
                   printer took a galley of type and scrambled it to make a type
                   specimen book.
                 </p>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
+                  typesetting industry. Lorem Ipsum has been the industry&apos;s
                   standard dummy text ever since the 1500s, when an unknown
                   printer took a galley of type and scrambled it to make a type
                   specimen book.
